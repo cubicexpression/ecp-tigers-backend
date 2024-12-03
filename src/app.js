7aa75dc882da66d77f0348388370379916ffd2c0
Tom Thistlethwaite <tomthistlethwaite@Toms-MacBook-Air.local>
2024-12-03 12:04:26 +0000

    fix port to 8080

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const axios = require('axios'); // Add this line
 
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors()); // Add this line
